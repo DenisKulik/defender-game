533143fe91066e33148d8209c45368d9e0744f0a
Denis Kulik <kulikdenis1996x@gmail.com>
2022-11-21 21:38:22 +0700

    add solution the math

---
+++ src/js/math-regexp.js
@@ -0,0 +1,57 @@
+import Character from './classes';
+
+export default class Math extends Character {
+  constructor(name) {
+    super(name);
+    this.defence = 40;
+  }
+
+  set stoned(flag) {
+    this._stoned = flag;
+  }
+
+  get stoned() {
+    return this._stoned;
+  }
+
+  set attack(value) {
+    this._attack = value;
+  }
+
+  get attack() {
+    if (!this.distance) {
+      throw new Error('The distance is not specified!');
+    }
+
+    if (this.distance < 0 || this.distance > 5) {
+      return 0;
+    }
+
+    let calculateDamage =
+      this._attack - (this._attack / 10) * (this.distance - 1);
+
+    if (this._stoned) {
+      calculateDamage -= Math.log2(this.distance) * 5;
+    }
+
+    return Math.floor(calculateDamage);
+  }
+}
+
+export class Daemon extends Math {
+  constructor(name) {
+    super(name);
+    this.type = 'Daemon';
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
+
+export class Magician extends Math {
+  constructor(name) {
+    super(name);
+    this.type = 'Magician';
+    this.attack = 10;
+    this.defence = 40;
+  }
+}
